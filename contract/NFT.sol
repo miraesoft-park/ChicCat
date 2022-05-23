e715df644516a836bd2ae67c5a7090f714a403b1
miraesoft-park <84835055+miraesoft-park@users.noreply.github.com>
2022-05-23 12:31:04 +0900

    Update NFT.sol

--- contract/NFT.sol
+++ contract/NFT.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract ChicCat is ERC721, Ownable {
