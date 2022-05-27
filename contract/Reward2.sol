324589f0d53a63ab891d8bb7b8f024b366498f00
miraesoft-park <84835055+miraesoft-park@users.noreply.github.com>
2022-05-27 16:23:20 +0900

    Update Reward2.sol

--- contract/Reward2.sol
+++ contract/Reward2.sol
@@ -5,7 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Reward2 is ERC20, ERC20Burnable, Ownable {
+contract Reward2 is ERC20, Ownable {
+// contract Reward2 is ERC20, ERC20Burnable, Ownable {
   using SafeMath for uint256;
 
   mapping(address => uint256) private _balances;
