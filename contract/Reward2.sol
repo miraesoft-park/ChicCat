549cf308c7d47c07396b78b97bb4ee9d59b2f4b8
miraesoft-park <84835055+miraesoft-park@users.noreply.github.com>
2022-05-27 16:15:25 +0900

    Update Reward2.sol

--- contract/Reward2.sol
+++ contract/Reward2.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT LICENSE
 
-pragma solidity ^0.8.4;
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
