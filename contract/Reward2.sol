7a0e7f82a368cc1a7b119e90b99e52cdbc17d78b
miraesoft-park <84835055+miraesoft-park@users.noreply.github.com>
2022-05-27 16:40:36 +0900

    Update Reward2.sol

--- contract/Reward2.sol
+++ contract/Reward2.sol
@@ -2,7 +2,6 @@
 
 pragma solidity 0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
